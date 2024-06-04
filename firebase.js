e72915610330d46f33e1ac6fc4bcef66508bbd6b
Naveen <naveenede4@gmail.com>
2024-06-04 11:27:54 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,47 @@
+// var firebaseConfig = {
+//     apiKey: "AIzaSyDzrFP40titNsQXqPN_W5lGqTNiEP0y9Dw",
+//     authDomain: "login-authentication-1b330.firebaseapp.com",
+//     projectId: "login-authentication-1b330",
+//     storageBucket: "login-authentication-1b330.appspot.com",
+//     messagingSenderId: "452246451383",
+//     appId: "1:452246451383:web:866c983e5eb78be6ff4932",
+//     measurementId: "G-FKH875ZC3M"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+var firebaseConfig = {
+    apiKey: "AIzaSyAWAHhJoHsNWRMpig3mJLI-oQRapiDy2KI",
+    authDomain: "movieplaylist-wolkus.firebaseapp.com",
+    projectId: "movieplaylist-wolkus",
+    storageBucket: "movieplaylist-wolkus.appspot.com",
+    messagingSenderId: "273162098716",
+    appId: "1:273162098716:web:b0504f60545b1540e267d9",
+    measurementId: "G-TWL5GCXMVF"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+ firebase.analytics();
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAWAHhJoHsNWRMpig3mJLI-oQRapiDy2KI",
+//     authDomain: "movieplaylist-wolkus.firebaseapp.com",
+//     projectId: "movieplaylist-wolkus",
+//     storageBucket: "movieplaylist-wolkus.appspot.com",
+//     messagingSenderId: "273162098716",
+//     appId: "1:273162098716:web:b0504f60545b1540e267d9",
+//     measurementId: "G-TWL5GCXMVF"
+//   };
+
+//   // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app);
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
